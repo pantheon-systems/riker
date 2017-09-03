01c5b5afa0a3c2f4c7bcab8de4401564e597668d
joe miller <joeym@joeym.net>
2017-09-03 08:20:11 -0700

    add initial example for cli-wrapper

---
+++ redshirts/cli-wrapper/examples/longcmd.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+echo hello from line 1
+echo sleep 6
+sleep 6
+echo hello from line 2
+echo hello from line 3
+echo hello from line 4
+echo hello from line 5
+echo sleep 5
+sleep 5
+echo hello from line 6
+echo hello from line 7
+echo hello from line 8
+echo hello from line 9
+echo sleep 3
+sleep 3
+echo hello from line 10
+echo hello from line 11
+echo hello from line 12
+echo hello from line 13
+echo hello from line 14
+echo hello from line 15
+echo hello from line 16
+echo hello from line 17
+echo hello from line 18
+echo hello from line 19
+echo hello from line 20
+echo hello from line 21
+echo sleep 2
+sleep 2
+echo hello from line 22
+echo hello from line 23
