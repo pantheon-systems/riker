457eb635171ddce3d9ea9f7c84af18360250186b
Eduard Bulai <eduard.bulai@gmail.com>
2019-05-13 19:22:35 +0300

    helper package

---
+++ pkg/helpers/helpers.go
@@ -0,0 +1,39 @@
+// Copyright © 2019 Pantheon
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package helpers
+
+import (
+	"os"
+	"strconv"
+)
+
+// Simple helper function to read an environment or return a default value
+func GetEnv(key string, defaultVal string) string {
+	if value, exists := os.LookupEnv(key); exists {
+		return value
+	}
+
+	return defaultVal
+}
+
+// Simple helper function to read an environment variable into integer or return a default value
+func GetEnvAsInt(name string, defaultVal int) int {
+	valueStr := GetEnv(name, "")
+	if value, err := strconv.Atoi(valueStr); err == nil {
+		return value
+	}
+
+	return defaultVal
+}
